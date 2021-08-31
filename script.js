1ccbbd9885edaddca18a43982a6ca75f900c3794
Nbkrhumor <89859649+Nbkrhumor@users.noreply.github.com>
2021-08-31 21:07:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function nextOne(){
+    let randomNumber3=Math.floor(Math.random()*25)+1;
+        document.querySelector(".img").setAttribute("src","Memes/img"   +randomNumber3+".jpg")
+        
+    }
