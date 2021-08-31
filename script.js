cc848731d7355033c5c10d27c04c1087402c5c4f
Nbkrhumor <89859649+Nbkrhumor@users.noreply.github.com>
2021-08-31 22:45:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function nextOne(){
-    let randomNumber3=Math.floor(Math.random()*25)+1;
+    let randomNumber3=Math.floor(Math.random()*120)+1;
         document.querySelector(".img").setAttribute("src","Memes/img"   +randomNumber3+".jpg")
         
     }
